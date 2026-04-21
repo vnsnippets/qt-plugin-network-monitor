3cb77d21d869f138905d4fdff6ebf3a522682da7
Vidush H. Namah <vnsnippets@users.noreply.github.com>
2026-04-21 21:57:52 +0400

    Add more state listeners Only tested the state changes so far (connected, disconnected etc.)

--- NetworkMonitor.cpp
+++ NetworkMonitor.cpp
@@ -1,27 +1,53 @@
-#include <gio/gio.h>   // GLib first
+#include <gio/gio.h>
 #include "NetworkMonitor.h"
 #include <QDebug>
+#undef signals   // avoid clash
 
-// Avoid macro clash
-#undef signals
-
-static void on_nm_state_changed(GDBusConnection *conn,
-                                const gchar *,
-                                const gchar *,
-                                const gchar *,
-                                const gchar *,
-                                GVariant *parameters,
-                                gpointer user_data)
-{
+// --- Callbacks ---
+static void on_nm_state_changed(GDBusConnection*, const gchar*, const gchar*, const gchar*, const gchar*, GVariant* parameters, gpointer user_data) {
     auto monitor = static_cast<NetworkMonitor*>(user_data);
     guint32 new_state;
     g_variant_get(parameters, "(u)", &new_state);
-    monitor->setConnectivityState(new_state);
+    monitor->setGlobalState(new_state);
 }
 
-NetworkMonitor::NetworkMonitor(QObject *parent)
-    : QObject(parent)
-{
+static void on_nm_properties_changed(GDBusConnection*, const gchar*, const gchar*, const gchar*, const gchar*, GVariant* parameters, gpointer user_data) {
+    auto monitor = static_cast<NetworkMonitor*>(user_data);
+    const gchar *iface;
+    GVariant *changed;
+    GVariant *invalidated;
+    g_variant_get(parameters, "(&s@a{sv}@as)", &iface, &changed, &invalidated);
+
+    if (g_strcmp0(iface, "org.freedesktop.NetworkManager") == 0) {
+        GVariant *val = g_variant_lookup_value(changed, "Connectivity", NULL);
+        if (val) {
+            guint32 connectivity = g_variant_get_uint32(val);
+            monitor->setConnectivityState(connectivity);
+            g_variant_unref(val);
+        }
+    }
+    g_variant_unref(changed);
+    g_variant_unref(invalidated);
+}
+
+static void on_device_added(GDBusConnection*, const gchar*, const gchar*, const gchar*, const gchar*, GVariant* parameters, gpointer user_data) {
+    auto monitor = static_cast<NetworkMonitor*>(user_data);
+    const gchar *path;
+    g_variant_get(parameters, "(o)", &path);
+    emit monitor->deviceAdded(QString::fromUtf8(path));
+}
+
+static void on_device_removed(GDBusConnection*, const gchar*, const gchar*, const gchar*, const gchar*, GVariant* parameters, gpointer user_data) {
+    auto monitor = static_cast<NetworkMonitor*>(user_data);
+    const gchar *path;
+    g_variant_get(parameters, "(o)", &path);
+    emit monitor->deviceRemoved(QString::fromUtf8(path));
+}
+
+// Similar callbacks can be added for ActiveConnection and Settings signals…
+
+// --- Constructor ---
+NetworkMonitor::NetworkMonitor(QObject *parent) : QObject(parent) {
     GError *error = nullptr;
     GDBusConnection *conn = g_bus_get_sync(G_BUS_TYPE_SYSTEM, nullptr, &error);
     if (!conn) {
@@ -30,16 +56,40 @@ NetworkMonitor::NetworkMonitor(QObject *parent)
         return;
     }
 
+    // Global signals
     g_dbus_connection_signal_subscribe(conn,
         "org.freedesktop.NetworkManager",
         "org.freedesktop.NetworkManager",
         "StateChanged",
         "/org/freedesktop/NetworkManager",
-        nullptr,
-        G_DBUS_SIGNAL_FLAGS_NONE,
-        on_nm_state_changed,
-        this,
-        nullptr);
+        nullptr, G_DBUS_SIGNAL_FLAGS_NONE,
+        on_nm_state_changed, this, nullptr);
+
+    g_dbus_connection_signal_subscribe(conn,
+        "org.freedesktop.NetworkManager",
+        "org.freedesktop.DBus.Properties",
+        "PropertiesChanged",
+        "/org/freedesktop/NetworkManager",
+        nullptr, G_DBUS_SIGNAL_FLAGS_NONE,
+        on_nm_properties_changed, this, nullptr);
+
+    g_dbus_connection_signal_subscribe(conn,
+        "org.freedesktop.NetworkManager",
+        "org.freedesktop.NetworkManager",
+        "DeviceAdded",
+        "/org/freedesktop/NetworkManager",
+        nullptr, G_DBUS_SIGNAL_FLAGS_NONE,
+        on_device_added, this, nullptr);
+
+    g_dbus_connection_signal_subscribe(conn,
+        "org.freedesktop.NetworkManager",
+        "org.freedesktop.NetworkManager",
+        "DeviceRemoved",
+        "/org/freedesktop/NetworkManager",
+        nullptr, G_DBUS_SIGNAL_FLAGS_NONE,
+        on_device_removed, this, nullptr);
+
+    // TODO: Add subscriptions for Device.StateChanged, ActiveConnection.StateChanged, Settings.NewConnection, etc.
 
     // Fetch initial Connectivity property
     GVariant *result = g_dbus_connection_call_sync(
@@ -65,10 +115,19 @@ NetworkMonitor::NetworkMonitor(QObject *parent)
     }
 }
 
+// --- Setters ---
 void NetworkMonitor::setConnectivityState(int state) {
     if (m_connectivityState != state) {
         m_connectivityState = state;
-        qDebug() << "Connectivity state updated:" << state;
+        qDebug() << "Connectivity updated:" << state;
         emit connectivityStateChanged();
+    }
+}
+
+void NetworkMonitor::setGlobalState(int state) {
+    if (m_globalState != state) {
+        m_globalState = state;
+        qDebug() << "Global NM state updated:" << state;
+        emit globalStateChanged();
     }
 }
--- NetworkMonitor.h
+++ NetworkMonitor.h
@@ -1,19 +1,31 @@
 #pragma once
-
 #include <QObject>
 
 class NetworkMonitor : public QObject {
     Q_OBJECT
     Q_PROPERTY(int connectivityState READ connectivityState NOTIFY connectivityStateChanged)
+    Q_PROPERTY(int globalState READ globalState NOTIFY globalStateChanged)
 
 public:
     explicit NetworkMonitor(QObject *parent = nullptr);
+
     int connectivityState() const { return m_connectivityState; }
-    void setConnectivityState(int state);   // make public
+    int globalState() const { return m_globalState; }
+
+    void setConnectivityState(int state);
+    void setGlobalState(int state);
 
 signals:
     void connectivityStateChanged();
+    void globalStateChanged();
+    void deviceAdded(const QString &path);
+    void deviceRemoved(const QString &path);
+    void deviceStateChanged(const QString &path, int state);
+    void activeConnectionStateChanged(const QString &path, int state);
+    void newConnection(const QString &path);
+    void connectionRemoved(const QString &path);
 
 private:
     int m_connectivityState = 0;
+    int m_globalState = 0;
 };
